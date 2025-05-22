40a1449a8465c0837a11d39942758de116067534
LuyandraBranco <luyandrabranco@gmail.com>
2025-05-22 22:28:55 +0100

    ♻️ refactor: lending options

---
+++ constants/lending-options.ts
@@ -0,0 +1,24 @@
+export const LENDINGOPTIONS = [
+  {
+    platform: 'Nemo',
+    imageUrl: '/blizzard-nft.png',
+    assetIconA: '/blizzard-nft.png',
+    assetIconB: '/blizzard-nft.png',
+    asset: 'WAL-SUI',
+    apr: '2.21%',
+    tvl: '$66.4K',
+    category: 'Lending',
+    points: '0.033/WAL/day',
+  },
+  {
+    platform: 'Bucket',
+    imageUrl: '/blizzard-nft.png',
+    assetIconA: '/blizzard-nft.png',
+    assetIconB: '/blizzard-nft.png',
+    asset: 'WAL-SUI',
+    apr: '2.21%',
+    tvl: '$66.4K',
+    category: 'Lending',
+    points: '',
+  },
+];
