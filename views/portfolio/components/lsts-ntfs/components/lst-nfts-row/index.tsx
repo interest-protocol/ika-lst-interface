5881a0dad671ec8105e6b3588bbd86694a00c113
luyandra.branco <luyandrabranco@gmail.com>
2025-05-26 18:04:30 +0100

    🚨 fix: typescript erros

--- views/portfolio/components/lsts-ntfs/components/lst-nfts-row/index.tsx
+++ views/portfolio/components/lsts-ntfs/components/lst-nfts-row/index.tsx
@@ -1,18 +1,9 @@
 import { Div } from '@stylin.js/elements';
 import { FC } from 'react';
 
-import LSTNTFsRowMetadata from './lst-nfts-row-metadata';
-import LSTNTFsRowMetrics from './lst-nfts-row-metrics';
-import LSTNTFsRowMetricsMobile from './lst-nfts-row-metrics-mobile';
 import { LSTNFTsRowProps } from './lsts-row.types';
 
-const LSTNFTsRow: FC<LSTNFTsRowProps> = ({
-  symbol,
-  iconUrl,
-  balance,
-  price,
-  value,
-}) => {
+const LSTNFTsRow: FC<LSTNFTsRowProps> = () => {
   return (
     <Div
       cursor="pointer"
@@ -27,21 +18,10 @@ const LSTNFTsRow: FC<LSTNFTsRowProps> = ({
       nHover={{ borderColor: '#FFFFFF33', bg: '#EE2B5B40' }}
     >
       <Div display={['block', 'block', 'none']} width="100%">
-        <Div display="flex" alignItems="center" gap="0.5rem" mb="0.75rem">
-          <LSTNTFsRowMetadata symbol={symbol} iconUrl={iconUrl} />
-        </Div>
-        <LSTNTFsRowMetricsMobile
-          balance={balance}
-          price={price}
-          value={value}
-        />
+        <Div display="flex" alignItems="center" gap="0.5rem" mb="0.75rem"></Div>
       </Div>
 
-      <Div display={['none', 'none', 'contents']}>
-        <LSTNTFsRowMetadata symbol={symbol} iconUrl={iconUrl} />
-
-        <LSTNTFsRowMetrics balance={balance} price={price} value={value} />
-      </Div>
+      <Div display={['none', 'none', 'contents']}></Div>
     </Div>
   );
 };
--- views/portfolio/components/lsts-ntfs/components/lst-nfts-row/lsts-row.types.ts
+++ views/portfolio/components/lsts-ntfs/components/lst-nfts-row/lsts-row.types.ts
@@ -1,7 +1,8 @@
 export interface LSTNFTsRowProps {
   name: string;
   subtitle: string;
-  iconBg: string;
+  iconUrl?: string;
+  symbol?: string;
   totalStaked: string;
   toWithdraw: string;
   status: string;
